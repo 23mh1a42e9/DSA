62889dbb021403b7f2a1c0369fb1f1daf12a0553
kadagala Ramanjayanaidu <23mh1a42e9@acoe.edu.in>
2025-09-14 14:37:59 +0530

    Time: 4 ms (17.71%) | Memory: 97.3 MB (86.82%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice=INT_MAX,profit=0;
+        for(int p:prices){
+            minprice=min(minprice,p);
+            profit=max(profit,p-minprice);
+        }
+        return profit;
+    }
+};
