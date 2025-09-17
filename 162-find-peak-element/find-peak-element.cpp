3163c28c1165269e70e1c2c8e6bd122c7df03b30
kadagala Ramanjayanaidu <23mh1a42e9@acoe.edu.in>
2025-09-17 18:17:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (67.29%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -3,35 +3,29 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
        int n=nums.size();
-       // Edge case: A single element array is a peak.
        if(n == 1) {
            return 0;
        }
-       // Check if the first element is a peak.
        if(nums[0] > nums[1]) {
            return 0;
        }
-       // Check if the last element is a peak.
        if(nums[n-1] > nums[n-2]) {
            return n-1;
        }
-       // Binary search for a peak in the middle of the array.
        int low=1, high=n-2;
        while(low <= high){
-            int mid= low + (high-low)/2; // Prevents integer overflow
+            int mid= low + (high-low)/2; 
             if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
                 return mid;
             }
-            // If the element to the left is greater, a peak must be in the left half.
-            else if(nums[mid-1] > nums[mid]) { 
-                high = mid - 1;
-            }
-            // If the element to the right is greater, a peak must be in the right half.
-            else { 
+          
+            else if(nums[mid] > nums[mid-1]) { 
                 low = mid + 1;
+            }
+            else { 
+                high = mid - 1;
            }
        }
-       // This line should technically be unreachable, as a peak is guaranteed to exist.
        return -1;
     }
 };
