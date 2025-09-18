ec141e89846a0d96f1c2dbbb2b1932fcd9c161e7
kadagala Ramanjayanaidu <23mh1a42e9@acoe.edu.in>
2025-09-18 22:31:44 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (93.64%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> nums;
-        nums.insert(nums.end(), nums1.begin(), nums1.end());
-        nums.insert(nums.end(), nums2.begin(), nums2.end());
-        sort(nums.begin(), nums.end());
-
-        int n = nums.size();
-        if (n % 2 == 0) {
-            return (static_cast<double>(nums[n / 2 - 1]) + nums[n / 2]) / 2.0;
-        } else {
-            return static_cast<double>(nums[n / 2]);
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
+        int m = nums1.size();
+        int n = nums2.size();
+        int low = 0;
+        int high = m;
+        int halfLen = (m + n + 1) / 2;
+
+        while (low <= high) {
+            int i = low + (high - low) / 2;
+            int j = halfLen - i;            
+
+            int maxLeftX = (i == 0) ? INT_MIN : nums1[i - 1];
+            int minRightX = (i == m) ? INT_MAX : nums1[i];
+
+            int maxLeftY = (j == 0) ? INT_MIN : nums2[j - 1];
+            int minRightY = (j == n) ? INT_MAX : nums2[j];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((m + n) % 2 == 1) {
+                    return std::max(maxLeftX, maxLeftY);
+                } else {
+                    return (static_cast<double>(std::max(maxLeftX, maxLeftY)) + std::min(minRightX, minRightY)) / 2.0;
+                }
+            } else if (maxLeftX > minRightY) {
+                high = i - 1;
+            } else {
+                low = i + 1;
+            }
+        }
+        return 0.0; 
     }
 };
