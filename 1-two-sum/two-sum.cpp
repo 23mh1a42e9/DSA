9847c21ae0de3ceffe3d53c5bfd391df664930d7
kadagala Ramanjayanaidu <23mh1a42e9@acoe.edu.in>
2025-09-21 10:31:50 +0530

    Time: 3 ms (69.94%) | Memory: 15.1 MB (13.25%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp; // ele , index
         for(int i=0;i<nums.size();i++){
-            for (int j=i+1;j<nums.size();j++){
-                if(nums[i] + nums[j] ==target){
-                    return {i,j};
-                }
+            int remain = target - nums[i];
+            if(mpp.find(remain) != mpp.end()){
+                return {mpp[remain] ,i};
             }
+            mpp[nums[i]] = i;
         }
         return {};
     }
