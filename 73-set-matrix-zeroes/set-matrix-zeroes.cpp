d530d5242f394cca5314e13a0e58b6cfdf8ca9b1
kadagala Ramanjayanaidu <23mh1a42e9@acoe.edu.in>
2025-09-21 10:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (51.55%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size() , m = matrix[0].size();
-        vector<int> row(n,0) , col(m,0);
+        vector<int> drow(n,0) ,dcol(m,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+                    drow[i] = 1;
+                    dcol[j] = 1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(row[i] == 1 || col[j] == 1) {
+                if(drow[i] == 1 || dcol[j] == 1) {
                     matrix[i][j] = 0;
                 }
             }
